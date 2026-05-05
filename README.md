0fd7654d973da86018bc76da018da70e62a246c7
dasadishree <dasadishree@gmail.com>
2026-05-05 04:33:07 -0400

    hero & navabr

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
+INSPO: https://www.instagram.com/business_uuncovered/
+
+
 ## Getting Started
 
 First, run the development server:
---
+++ app/components/Hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+    return(
+        <section className="hero">
+            <div className="hero-bg"></div>
+            <div className="hero-content">
+                <h1>
+                    <span className="hero-title">business</span>
+                    <br></br>
+                    <span className="hero-title-block">UNCOVERED</span>
+                </h1>
+                <p className="hero-subtitle">The Heart of Business</p>
+            </div>
+        </section>
+    )
+}
